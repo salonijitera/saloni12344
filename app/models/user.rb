6183b3e5dbd6d2af976fad31c2302f58aa53adad
Cheryl (Jitera) <developers@jitera.com>
2024-01-16 12:03:39 +0000

    fix: 1 issues from deployment report

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,10 @@
+
 class User < ApplicationRecord
   has_one :email_verification_token, dependent: :destroy
+  has_many :email_verification_tokens
 
   # validations
-
+  validates_presence_of :email, :password
   # end for validations
 
   class << self
